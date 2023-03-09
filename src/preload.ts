24444abfd159ccd0d8231d4e1137eb7d1e62cba5
Enrike Churin <52293637+GGIEnrike@users.noreply.github.com>
2023-03-09 21:38:19 +0100

    Organize preload api; add fullscreen event

--- src/preload.ts
+++ src/preload.ts
@@ -2,11 +2,18 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
+    // Handlers
     handlePageTitleUpdated: (callback: () => void) => ipcRenderer.on('page-title-updated', callback),
     handlePageURLUpdated: (callback: () => void) => ipcRenderer.on('page-url-updated', callback),
-    setUrl: (url: string) => ipcRenderer.send('set-url', url),
-    
-    focusView: () => ipcRenderer.send('focus-view'),
+
+    handleFullscreenEntered: (callback: () => void) => ipcRenderer.on('fullscreen-entered', callback),
+    handleFullscreenLeft: (callback: () => void) => ipcRenderer.on('fullscreen-left', callback),
+
     handleSetThemeColor: (callback: () => void) => ipcRenderer.on('set-theme-color', callback),
+
+    // Setters
+    setUrl: (url: string) => ipcRenderer.send('set-url', url),
+
+    focusView: () => ipcRenderer.send('focus-view'),
 })
 
